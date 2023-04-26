e641e86eb0216a48883cef6936089eadc099e572
BogdanAburame <my_project2020@inbox.ru>
2023-04-26 18:42:58 +0300

    hide

--- src/api/HotelApi.ts
+++ src/api/HotelApi.ts
@@ -9,35 +9,31 @@ interface IHotelsAPI {
 export const HotelsApi: IHotelsAPI = {
   currentIndex: 0,
   async getHotels() {
-    const response = await axios
-      .request({
-        method: "POST",
-        url: "https://hotels4.p.rapidapi.com/properties/v2/list",
-        headers: {
-          "content-type": "application/json",
-          "X-RapidAPI-Key":
-            import.meta.env.VITE_API_KEY,
-          "X-RapidAPI-Host": import.meta.env.VITE_HOST_KEY,
-        },
-        data: `{"currency":"USD","eapid":1,"locale":"en_US","siteId":300000001,"destination":{"regionId":""},"checkInDate":{"day":10,"month":10,"year":2022},"checkOutDate":{"day":15,"month":10,"year":2022},"rooms":[{"adults":2}],"resultsStartingIndex":${this.currentIndex},"resultsSize":30,"sort":"PRICE_LOW_TO_HIGH"}`,
-      })
-      this.currentIndex += 30;
-      return response.data.data.propertySearch.properties
+    const response = await axios.request({
+      method: "POST",
+      url: "https://hotels4.p.rapidapi.com/properties/v2/list",
+      headers: {
+        "content-type": "application/json",
+        "X-RapidAPI-Key": import.meta.env.VITE_API_KEY,
+        "X-RapidAPI-Host": import.meta.env.VITE_HOST_KEY,
+      },
+      data: `{"currency":"USD","eapid":1,"locale":"en_US","siteId":300000001,"destination":{"regionId":""},"checkInDate":{"day":10,"month":10,"year":2022},"checkOutDate":{"day":15,"month":10,"year":2022},"rooms":[{"adults":2}],"resultsStartingIndex":${this.currentIndex},"resultsSize":30,"sort":"PRICE_LOW_TO_HIGH"}`,
+    })
+    this.currentIndex += 30
+    return response.data.data.propertySearch.properties
   },
   async loadMoreHotels() {
-    const response = await axios
-      .request({
-        method: "POST",
-        url: "https://hotels4.p.rapidapi.com/properties/v2/list",
-        headers: {
-          "content-type": "application/json",
-          "X-RapidAPI-Key":
-            import.meta.env.VITE_API_KEY,
-          "X-RapidAPI-Host": import.meta.env.VITE_HOST_KEY,
-        },
-        data: `{"currency":"USD","eapid":1,"locale":"en_US","siteId":300000001,"destination":{"regionId":""},"checkInDate":{"day":10,"month":10,"year":2022},"checkOutDate":{"day":15,"month":10,"year":2022},"rooms":[{"adults":2}],"resultsStartingIndex":${this.currentIndex},"resultsSize":30,"sort":"PRICE_LOW_TO_HIGH"}`,
-      })
-      this.currentIndex += 30;
-      return response.data.data.propertySearch.properties
-  }
+    const response = await axios.request({
+      method: "POST",
+      url: "https://hotels4.p.rapidapi.com/properties/v2/list",
+      headers: {
+        "content-type": "application/json",
+        "X-RapidAPI-Key": import.meta.env.VITE_API_KEY,
+        "X-RapidAPI-Host": import.meta.env.VITE_HOST_KEY,
+      },
+      data: `{"currency":"USD","eapid":1,"locale":"en_US","siteId":300000001,"destination":{"regionId":""},"checkInDate":{"day":10,"month":10,"year":2022},"checkOutDate":{"day":15,"month":10,"year":2022},"rooms":[{"adults":2}],"resultsStartingIndex":${this.currentIndex},"resultsSize":30,"sort":"PRICE_LOW_TO_HIGH"}`,
+    })
+    this.currentIndex += 30
+    return response.data.data.propertySearch.properties
+  },
 }
